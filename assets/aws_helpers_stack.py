e74c2aaf3b78c406546abc6430997f4c2e9f98a3
BhaKot <Bharat.oradev46@gmail.com>
2024-07-01 12:19:28 -0500

    remove conditonal check

--- assets/aws_helpers_stack.py
+++ assets/aws_helpers_stack.py
@@ -49,74 +49,29 @@ class AwsHelpersStack(Stack):
             role_arn="arn:aws:iam::527571104735:role/service-role/GAR_QA_SF_INFRA_LAMBDA-role-jfg7b4jv",
         )
 
-        # Check if the Lambda function already exists
-        existing_lambda_function = _lambda.Function.from_function_arn(
+        # Defines the AWS Lambda resource (for v2)
+        gar_qa_sf_infra_lambda_v2 = _lambda.Function(
             self,
-            "ExistingLambdaFunctionV2",
-            "arn:aws:lambda:us-east-1:527571104735:function:gar_qa_sf_infra_consumer_v2",
+            "gar_qa_sf_infra_consumer_v2",
+            runtime=_lambda.Runtime.PYTHON_3_12,
+            code=_lambda.Code.from_asset("assets"),
+            handler="lambda_function.lambda_handler",
+            function_name="gar_qa_sf_infra_consumer_v2",
+            role=lambda_role,
+            description="My infra consumer lambda v2",
         )
 
-        # Conditionally update or create the Lambda function
-        if existing_lambda_function.function_arn is not None:
-            # Defines an AWS Lambda resource
-            try:
-                gar_qa_sf_infra_lambda = _lambda.Function(
-                    self,
-                    "gar_qa_sf_infra_consumer_v2",
-                    runtime=_lambda.Runtime.PYTHON_3_12,
-                    code=_lambda.Code.from_asset("assets"),
-                    handler="lambda_function.lambda_handler",
-                    function_name="gar_qa_sf_infra_consumer_v2",
-                    role=lambda_role,
-                    description="My infra consumer lambda v2",
-                )
+        logging.info(
+            f"Lambda function created/updated: {gar_qa_sf_infra_lambda_v2.function_name}"
+        )
 
-                logging.info(
-                    f"Lambda function created: {gar_qa_sf_infra_lambda.function_name}"
-                )
-            except Exception as e:
-                logging.error(f"Error creating Lambda function: {str(e)}")
-                raise
+        # Add SQS as an event source for the Lambda
+        gar_qa_sf_infra_lambda_v2.add_event_source(
+            SqsEventSource(fifo_queue, batch_size=1)
+        )
 
-            # Add SQS as an event source for the Lambda
-            gar_qa_sf_infra_lambda.add_event_source(
-                SqsEventSource(fifo_queue, batch_size=1)
-            )
-
-            logging.info(
-                f"Lambda function created: {gar_qa_sf_infra_lambda.function_name}"
-            )
-
-            logging.info(
-                f"SQS queue : {fifo_queue.queue_name} created and set as trigger for Lambda"
-            )
-        else:
-            # Defines an AWS Lambda resource
-            try:
-                # Define the custom resource to update the Lambda function's code
-                """lambda_update_code = cr.AwsCustomResource(
-                    self,
-                    "UpdateLambdaCode",
-                    on_create=cr.AwsSdkCall(
-                        service="Lambda",
-                        action="updateFunctionCode",
-                        parameters={
-                            "FunctionName": existing_lambda_function.function_arn,
-                            "ZipFile": "<base64-encoded-zip-file>"
-                        },
-                        code = _lambda.Code.from_asset("assests")
-                        physical_resource_id=cr.PhysicalResourceId.of("UpdateLambdaCodeResourceId")
-                    ),
-                    policy=cr.AwsCustomResourcePolicy.from_sdk_calls(
-                        resources=cr.AwsCustomResourcePolicy.ANY_RESOURCE
-                    )
-                )"""
-
-                logging.info(
-                    f"Lambda function updated: {gar_qa_sf_infra_lambda.function_name}"
-                )
-            except Exception as e:
-                logging.error(f"Error updating Lambda function: {str(e)}")
-                raise
+        logging.info(
+            f"SQS queue : {fifo_queue.queue_name} created and set as trigger for Lambda v2"
+        )
 
         logging.info("AwsHelpersStack initialization complete")
